bccb9aeff0137a79ebdc0896ba563e19dd5d7241
Qudecim <99prblmsaint1@gmail.com>
2022-12-02 17:50:00 +0300

    _

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,16 +16,16 @@ class AuthController extends Controller
      */
     public function register(Request $request): User
     {
-        $attr = $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users,email',
             'password' => 'required|string|min:6|confirmed'
         ]);
 
         return User::create([
-            'name' => $attr['name'],
-            'password' => bcrypt($attr['password']),
-            'email' => $attr['email']
+            'name' => $validatedData['name'],
+            'password' => bcrypt($validatedData['password']),
+            'email' => $validatedData['email']
         ]);
     }
 
@@ -37,12 +37,12 @@ class AuthController extends Controller
      */
     public function login(Request $request): User
     {
-        $attr = $request->validate([
+        $validatedData = $request->validate([
             'email' => 'required|string|email|',
             'password' => 'required|string|min:6'
         ]);
 
-        if (!Auth::attempt($attr)) {
+        if (!Auth::attempt($validatedData)) {
             throw new \Exception('Credentials not match');
         }
 
