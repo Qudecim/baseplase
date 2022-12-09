a3905ae0c7672d0858aa10482f3715208ebcdb80
Qudecim <99prblmsaint1@gmail.com>
2022-12-09 17:53:44 +0300

    _

--- app/Baceplace/PubSub/Broker.php
+++ app/Baceplace/PubSub/Broker.php
@@ -14,6 +14,24 @@ class Broker
      */
     public static function filter(string $className, mixed $data): void
     {
+        $actions = auth()->user()->actions()->where('publisher', $className)->get();
+        foreach ($actions as $action) {
+            $valid = true;
+
+            foreach ($action as $key => $value) {
+                if (!isset($data->$key)) {
+                    $valid = false;
+                    continue;
+                }
+                if ($data->$key != $value) {
+                    $valid = false;
+                    continue;
+                }
+            }
+
+
+        }
+
         dd([$className, $data]);
     }
 
